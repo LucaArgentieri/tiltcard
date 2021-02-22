22e40725bc93e781325ada23737b0f945f11b17e
LucaArgentieri <Lucaargentieri-98@hotmail.it>
2021-02-22 12:04:07 +0100

    Update

--- index.js
+++ index.js
@@ -1,48 +1,62 @@
 const card = document.querySelector('.card')
+const cards = document.querySelectorAll('.card')
+
 
 function map(value, low1, high1, low2, high2) {
     return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
 }
 
+class cardElement {
+    constructor() {
+        this.cards = cards
+    }
+    init() {
+        this.cards.forEach(el => {
 
-card.addEventListener('mouseover', evt => {
-    gsap.to(card, {
-        scale: 1.1,
-        duration: 0.1,
-        boxShadow: "0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)",
-    })
+            //Over
+            el.addEventListener('mouseover', evt => {
+                gsap.to(el, {
+                    scale: 1.1,
+                    duration: 0.1,
+                    boxShadow: "0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)",
+                })
+            })
 
-})
+            //Move
+            el.addEventListener('mousemove', evt => {
+                var rect = evt.target.getBoundingClientRect();
+                var x2 = evt.clientX - rect.left;
+                var y2 = evt.clientY - rect.top;
 
-card.addEventListener('mousemove', evt => {
-    const x = evt.clientX
-    const y = evt.clientY
-    const offX = evt.offsetX / 2
-    const offY = evt.offsetY / 2
+                const normalized = map(x2, 0, rect.width, -10, 10)
+
+                const normalizedY = map(y2, 0, rect.height, 10, -10)
+
+                gsap.to(el, {
+                    rotateY: normalized + "deg",
+                    rotateX: normalizedY + "deg"
+                })
+            })
+
+            //Leave
+            el.addEventListener('mouseleave', evt => {
+                gsap.to(el, {
+                    scale: 1,
+                    duration: '0.6',
+                    boxShadow: "0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24)",
+                    transform: "none",
+                    rotateY: "0deg",
+                    rotateX: "0deg"
+                })
+            })
+        })
 
 
-    const normalized = map(x, 0, window.innerWidth, -offY, offY)
+    }
+}
+
+const singleCard = new cardElement()
+singleCard.init()
 
 
-    const normalizedY = map(y, 0, window.innerHeight, offX, -offX)
-
-
-
-    gsap.to(card, {
-        rotateY: normalized + "deg",
-        rotateX: normalizedY + "deg"
-    })
-})
-
-
-card.addEventListener('mouseleave', evt => {
-    gsap.to(card, {
-        scale: 1,
-        duration: '0.6',
-        boxShadow: "0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24)",
-        transform: "none",
-        rotateY: "0deg",
-        rotateX: "0deg"
-    })
-})
 
